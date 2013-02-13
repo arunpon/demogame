0ff2b691aa930348f9050f08dea24b7ef391b79e
Arun Ponnusamy <aponnusa@adobe.com>
2013-02-12 21:03:59 -0800

    Fixing node server

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
-var express = require('express'),
-  app = express()
 
-// Configuration
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
 app.configure(function(){
-  app.use(express.static(__dirname + '/public'));
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-
-  //Error Handling
-  app.use(express.logger());
-  app.use(express.errorHandler({
-    dumpExceptions: true, 
-    showStack: true
-  }));
-  
   app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+//app.get('/', routes.index);
 app.get('/', function(req, res){
   res.redirect("/index.html");
 });
+app.get('/users', user.list);
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
 });
